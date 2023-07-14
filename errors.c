87e200597c563259b1d5922542151b8f4b929882
me12free <johnewoi72@gmail.com>
2023-07-14 11:19:49 -0700

    Error and README files update

---
+++ errors.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints input string
+ * @str: string to be printed
+ * Return: 0
+ */
+
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - write the char c to the stderr
+ * @c: char to print
+ * Return: 1 on success
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf(i++) = c;
+	return (i);
+}
+
+/**
+ * _putsfd - prints an input string
+ * @str: string to e printed
+ * @fd: file descriptor to write to
+ * Return: no of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+		return (i);
+	}
+}
