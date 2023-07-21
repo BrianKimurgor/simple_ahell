37dcd8638a432a5da4ce55a9549db9b0277045ab
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-07-20 23:30:31 -0700

    files added

---
+++ environ.c
@@ -0,0 +1,96 @@
+#include "shell.h"
+
+/**
+ * _myenx - prints current environment
+ * @info: structure with potential arguments,
+ * for maintaining constant function prototype
+ * Return: 0
+ */
+
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _getenv - gets thr value of environ variable
+ * @info: structur with potential arguments,
+ * to maintain constant function prototype
+ * @name: env var name
+ * Return: value
+ */
+
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - initialize or modify new environment variable
+ * @info: structure with potential arguments
+ * to maintain constant function prototype
+ * Return: 0
+ */
+
+int _mysetenv(info_t *info)
+{
+	info (info->argc != 3)
+	{
+		_eputs("incorrect number of arguments\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - remove an environment variable
+ * @info: structure with potential arguments
+ * for constant function prototype maintenance
+ * Return: 0
+ */
+
+int_myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("too few arguments.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: structure containing arguments
+ * to maintain constant function prototype
+ * Return: 0
+ */
+
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node,environ[i], 0);
+	info->env = node;
+	return (0);
+}
