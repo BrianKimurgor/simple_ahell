31f825759da134ae0fe45e7bb305dbb8ec696c5d
me12free <johnewoi72@gmail.com>
2023-07-26 06:20:34 -0700

    Debbuging

--- string.c
+++ string.c
@@ -51,7 +51,7 @@ int _strcmp(char *s1, char *s2)
 char *starts_with(const char *haystack, const char *needle)
 {
 	while (needle)
-		if (*need;le++ != *haystack++)
+		if (*needle++ != *haystack++)
 			return (NULL);
 	return ((char *)haytack);
 }
